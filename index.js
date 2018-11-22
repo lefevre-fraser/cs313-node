e332daabde2f3f764c370d2660ec2315a9db5593
Fraser LeFevre <mp3lefevre@gmail.com>
2018-11-21 17:17:56 -0700

    Teach 10

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const { Pool } = require('pg')
 const pool = new Pool({
-  connectString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: true
 })
 const PORT = process.env.PORT || 5000
@@ -50,13 +50,11 @@ express()
       const result = await client.query('select * from person where person_id = ' + id)
       const results = { 'results': (result) ? result.rows : null }
       res.render('../public/Teach10/Teach10.ejs', results)
-      res.end()
       client.release()
     } catch (err) {
       console.error(err)
       res.send("ERROR: " + err)
     }
-
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
