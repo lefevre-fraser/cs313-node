2d41229c76f0da179d428b7c4b00d60eedc351c5
Fraser LeFevre <mp3lefevre@gmail.com>
2018-11-24 17:38:59 -0700

    Asset Tracker

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ express()
   })
   .post('/AssetTracker/login', async (req, res) => {
     const client = await pool.connect()
-    var query = "select user_id, user_name from users where user_name = $1::string"
+    var query = "select user_id, user_name from users where user_name = $1::varchar"
     const result = await client.query(query, [req.query.user_name])
     req.session.user_name = results.rows[0].user_name;
 
